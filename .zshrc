5280aa98a2663d2c1a11cb1ab65c89766be7ed75
Math Ha Sum <m.hasum@alohi.com>
2023-10-25 16:17:54 +0200

    :sparkles: Add goto

--- .zshrc
+++ .zshrc
@@ -16,7 +16,7 @@ plugins=(
 )
 
 source $ZSH/oh-my-zsh.sh
-source $ZSH/goto.sh
+source $ZSH/custom/goto.sh
 
 # User configuration
 export PATH="$HOME/go/bin/:$PATH"
--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ zshrc() {
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     git clone --depth=1 https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
-    git clone --depth=1 https://github.com/iridakos/goto.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/goto
+    git clone --depth=1 https://github.com/iridakos/goto.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/goto
 
     cat .zshrc > $HOME/.zshrc
     cat .p10k.zsh > $HOME/.p10k.zsh
